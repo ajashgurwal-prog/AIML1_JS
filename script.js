60bc940be9c7777f48737dc06b9c01a27500fc31
Ajay Gupta <ajashgurwal@gmail.com>
2026-02-20 10:33:37 +0530

    second commit

--- script.js
+++ script.js
@@ -1,2 +1,44 @@
-console.log("Hello World");
-document.write("Hello World");
+//console.log("Hello World");
+//document.write("Hello World");
+<>//console.log("Hello World");
+    //document.write("Hello World");
+    <script type="text/javascript">
+        var num = 10;
+        console.log(num);
+        console.log(typeof num);
+        num = false;
+        console.log(num);
+        console.log(typeof num);
+        num = "ajay gupta";
+        console.log(num);
+        console.log(typeof num);
+        num = null;
+        console.log(num);
+        console.log(typeof num);
+        num = undefined;
+        console.log(num);
+        console.log(typeof num);
+    </script><script type="text/javascript">
+//function
+         var sayHello = function() {alert("Hello World")};
+        
+        //array
+        var numArray = [1, 2, 3, 4, 5];
+        var animal = new Array("dog", "cat", "mouse");
+        //object
+        var person = {name: "barack joe khan",
+        age: 60,
+        title: "plumber"
+        }
+    </script>
+    <script type="text/javascript">
+    const sym1 = Symbol(4)
+    console.log(sym1);
+    const sym2 = Symbol(4);
+    console.log(sym2);
+    if (sym1 === sym2) {
+        console.log("sym1 and sym2 are equal");
+    } else {
+        console.log("sym1 and sym2 are not equal");
+    }
+    </script>
